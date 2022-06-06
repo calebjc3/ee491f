1fea022ac4af7b791f16bb89d8c410fa1e9a4729
calebjc3 <43564607+calebjc3@users.noreply.github.com>
2022-06-05 19:11:44 -1000

    Modifications

--- html-practice/main.js
+++ html-practice/main.js
@@ -1,15 +1,13 @@
 $(document).ready(function() {
-    console.log("ready!");
-    $("button").html("Click Here")
-    $("button").on("click", function( event ) {
-      $("p").toggle("slow");
-    });
+  console.log("ready!");
+  $("button").html("Click Here")
+  $("button").on("click", function( event ) {
+    ($("p").toggle("important"));
   });
 
-  $(document).ready(function() {
-    console.log("ready!");
-    $("toggle").html("Show Navbar")
-    $("toggle").on("click", function( event ) {
-      $("navbar").toggle("slow");
-    });
+  console.log("ready!");
+  $("#toggle").html("Show Navbar")
+  $("#toggle").on("click", function( event ) {
+    $("#toggle").toggle("slow");
   });
+}); 
